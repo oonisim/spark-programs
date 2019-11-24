94ed46a4284d34a38162642d0f96edb5f5c7eeb6
oonisim <oonisim@gmail.com>
2019-11-24 21:42:58 +1000

    Changed the spark parameter to avoid OOM.

--- Flight/src/main/jupyter/setup.sh
+++ Flight/src/main/jupyter/setup.sh
@@ -7,5 +7,13 @@ conda activate ${ENV}
 conda install jupyter notebook
 pip install --upgrade toree
 jupyter toree install --spark_home=${SPARK_HOME} --interpreters=Scala,PySpark,SQL --user
-SPARK_OPTS='--master=local[*]' jupyter notebook
+
+#------------------------------------------------------------------------------------------
+# Run Toree
+#------------------------------------------------------------------------------------------
+#SPARK_OPTS='--master=local[*]' jupyter notebook
+
+# OOM error
+#SPARK_OPTS='--master spark://masa:7077 --deploy-mode client --num-executors 4 --driver-memory 2g --executor-memory 2g --executor-cores 4' jupyter notebook
+SPARK_OPTS='--master spark://masa:7077 --deploy-mode client --num-executors 2 --driver-memory 4g --executor-memory 5g --executor-cores 4' jupyter notebook
 
