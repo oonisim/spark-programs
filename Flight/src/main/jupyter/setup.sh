47e1fd99864ad547070e87d4843fda209ee3abb5
oonisim <oonisim@gmail.com>
2019-12-09 20:57:44 +1000

    Chapter 07 aggregation primitives

--- Flight/src/main/jupyter/setup.sh
+++ Flight/src/main/jupyter/setup.sh
@@ -1,11 +1,13 @@
-#!/bin/bash
+echo "Source this file, not execute"
+
 set -eu
+export ENV=flight
 
 conda create -n ${ENV} python=3.6
 conda activate ${ENV}
 
 conda install jupyter notebook
-pip install --upgrade toree
+pip3 install --upgrade toree
 jupyter toree install --spark_home=${SPARK_HOME} --interpreters=Scala,PySpark,SQL --user
 
 #------------------------------------------------------------------------------------------
@@ -15,5 +17,5 @@ jupyter toree install --spark_home=${SPARK_HOME} --interpreters=Scala,PySpark,SQ
 
 # OOM error
 #SPARK_OPTS='--master spark://masa:7077 --deploy-mode client --num-executors 4 --driver-memory 2g --executor-memory 2g --executor-cores 4' jupyter notebook
-SPARK_OPTS='--master spark://masa:7077 --deploy-mode client --num-executors 2 --driver-memory 4g --executor-memory 5g --executor-cores 4' jupyter notebook
+SPARK_OPTS='--master spark://masa:7077 --deploy-mode client --num-executors 2 --driver-memory 2g --executor-memory 4g --executor-cores 4' jupyter notebook
 
